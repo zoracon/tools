f3762842e730ba2ba230146432c6e270129eef29
zoracon <alexis@eff.org>
2022-05-30 20:11:57 -0700

    Update with proper commands

--- cloudScan.sh
+++ cloudScan.sh
@@ -24,22 +24,26 @@ SCANMORE=$2
 if [ $SCANMORE ]; then
     #Output Port Scans to txt file to parse
     echo "Scanning ports 1-7000"
-    sudo masscan -iL $IPS -p1-7000 -oJ masscan1.json
+    sudo masscan -iL $IPS -p1-7000 -oJ masscan.json
 else 
     echo "Scanning ports 1-1024"
-    sudo masscan -iL $IPS -p1-1024 -oJ masscan1.json
+    sudo masscan -iL $IPS -p1-1024 -oJ masscan.json
 fi
  
-# remove last commma :/
+echo "Extracting Ports Found"
+sudo jq -c '.[] | .ports | .[].port' masscan.json | sed -z 's/\n/,/g;s/,$/\n/' >> ports.txt
 
-sudo jq '.[] | .ports | .[].port' masscan1.json >> ports.txt
-
-# Take parsed file with ports and enter command:
-# Take argument for output dir
+echo "Start nmap scan of ports"
 sudo nmap -iL ips.txt -p `cat ports.txt` -Pn -A -oA *
 
-# parse nmap scan for services...choose names and input into nmap scripts scan
-sudo nmap -iL ips.txt -p `cat ports.txt` -Pn -oA * -O -sV --script=[redis*,mongo*]
+echo "letting scan finish with a forced sleep"
 
+sleep 5
 
+echo "parsing services..."
+cat cloudScan.sh.xml | xq '.nmaprun.host.ports.port | .[].service."@name"' | sed -z 's/\n/*,/g;s/"//g' >> services.txt
 
+echo "Now try some scripts..." 
+# Input your scripts from services.txt
+# In the future maybe take user input here to execute, but good for a manual once over for now
+# sudo nmap -iL ips.txt -p `cat ports.txt` -Pn -oA * -O -sV --script=`cat services.txt`
